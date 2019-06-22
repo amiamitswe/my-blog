ab4dc9ad2555d6f1ff19b0bd625e40b71ce770a7
Amit Samadder <amiamitswe@gmail.com>
2019-06-22 21:27:00 +0600

    first commit

---
+++ js/init.js
@@ -0,0 +1,35 @@
+
+// navigator
+document.addEventListener('DOMContentLoaded', function() {
+  let elems = document.querySelectorAll('.sidenav');
+  let options = {} ;
+  let instances = M.Sidenav.init(elems, options);
+});
+
+
+// slider
+document.addEventListener('DOMContentLoaded', function() {
+  let elems = document.querySelectorAll('.slider');
+  let options = { duration: 500, interval:5000 } ;
+  let instances = M.Slider.init(elems, options);
+});
+
+
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
