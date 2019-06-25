a4fa48fb95f256d7fddd437557221a3e9580ebce
Amit Samadder <amiamitswe@gmail.com>
2019-06-25 20:39:55 +0600

    add little bit for responsive and nav bar fixt

--- js/init.js
+++ js/init.js
@@ -1,45 +1,57 @@
-
-// navigator
-document.addEventListener('DOMContentLoaded', function() {
-  let elems = document.querySelectorAll('.sidenav');
-  let options = {} ;
-  let instances = M.Sidenav.init(elems, options);
-});
-
-
-// slider
-document.addEventListener('DOMContentLoaded', function() {
-  let elems = document.querySelectorAll('.slider');
-  let options = { duration: 500, interval:5000 } ;
-  let instances = M.Slider.init(elems, options);
-});
-
-
-
-
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+    scrollFunction()
+};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("myBtn").style.display = "block";
-  } else {
-    document.getElementById("myBtn").style.display = "none";
-  }
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
 
+/////////////////////////////////
 // get browser clientHeight
 // alert(document.body.clientHeight);
-document.querySelector(".header").style.height = document.body.clientHeight + "px";
+// document.querySelector(".header").style.height = document.body.clientHeight + "px";
 
-document.addEventListener('DOMContentLoaded', function() {
-  let elems = document.querySelectorAll('.modal');
-  let options = {}
-  let instances = M.Modal.init(elems, options);
+
+document.addEventListener('DOMContentLoaded', function () {
+    let elems = document.querySelectorAll('.modal');
+    let options = {};
+    let instances = M.Modal.init(elems, options);
+});
+
+function showNav() {
+    let data = document.querySelector(".header");
+
+    if (data.style.display === "block") {
+        data.style.display = "none";
+
+    } else {
+        data.style.display = "block";
+    }
+}
+
+/////////////////////////////////
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+        direction: 'left',
+        hoverEnabled: false
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.tooltipped');
+    let options = {position: "top"};
+    var instances = M.Tooltip.init(elems, options);
 });
